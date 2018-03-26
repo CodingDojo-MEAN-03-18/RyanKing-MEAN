ab9307ff14208adbc6234fac63b76f407dea7b04
Ryan King <ryanmking62@gmail.com>
2018-03-26 15:51:27 -0700

    added math module

---
+++ node/math_module/app.js
@@ -0,0 +1,6 @@
+const math_module = require('./mathlib.js')();
+
+console.log(math_module.add(2,3));
+console.log(math_module.multiply(3,5));
+console.log(math_module.square(5));
+console.log(math_module.random(1, 35));
