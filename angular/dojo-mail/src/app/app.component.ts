d690572cb90972edd7211befabe34c99ab1c5848
Ryan King <ryanmking62@gmail.com>
2018-04-18 01:39:09 -0700

    added dojo mail

---
+++ angular/dojo-mail/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  messages = [
+    { email: 'bill@gates.com',
+      importance: true,
+      subject: 'New Windows',
+      content: 'Windows XI will launch in year 2100',
+    },
+    { email: 'ada@lovelace.com',
+      importance: true,
+      subject: 'Programming',
+      content: 'Enchantress of Numbers',
+    },
+    { email: 'john@carmac.com',
+      importance: false,
+      subject: 'Updated algo',
+      content: 'New algorithm for shadow volumes',
+    },
+    { email: 'Gabe@newell.com',
+      importance: false,
+      subject: 'HL3!',
+      content: 'Just kidding...',
+    }
+  ];
+}
